cbe1cf9ff273a5c891e2fa035e75c0c965f8b927
alexger95 <alexger95@gmail.com>
2020-02-27 22:43:24 +0300

    init: v1 solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    return !array || array.length < 1 ? 0 : sort(array).shift();
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return !array || array.length < 1 ? 0 : sort(array).pop();
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length < 1) {
+        return 0;
+    }
+    array = array.reduce(function(sum, current) {
+        return sum + current;
+    }) / array.length;
+    return array;
+}
+
+function sort(arr) {
+    arr = arr.sort(function(a, b) {
+        return a - b;
+    });
+    return arr;
 }
